e33d68f36e4289b5506e5f2414459646a818b0b5
Dmytro Zemliankin <dzemliankin@luxoft.com>
2020-04-16 22:30:38 +0200

    Associate response with request using JMSCorrelationID

--- README.md
+++ README.md
@@ -8,17 +8,21 @@ Before running application, ActiveMQ broker should be started.
 The simplest you can do is to run ActiveMQ in Docker.
 ```
 docker pull rmohr/activemq
-docker run -p 61616:61616 -p 8161:8161 rmohr/activemq
+docker run -it -p 61616:61616 -p 8161:8161 rmohr/activemq
 ```
 
-The ActiveMQ console can be used to play with calculator.
+The ActiveMQ console http://localhost:8161 can be used to play with calculator. Credentials are `admin:admin` by default.
 
 ## Messages format
 
 The messages are in JSON format.
 
 ### Request
-```json
+
+Requester is responsible to set JMSCorrelationID for further association with response.
+
+Message body:
+```
 {
     "operator": "String",
     "operands": [ Integer, Integer, ... ]
@@ -32,13 +36,17 @@ Example:
 ```
 
 ### Response
-```json
+
+JMSCorrelationID is used to associate response with request.
+
+Message body: 
+```
 {
     "result": Integer
 }
 ```
 or
-```json
+```
 {
     "error": "String"
 }
--- src/main/java/org/example/jmscalculator/JmsReceiver.java
+++ src/main/java/org/example/jmscalculator/JmsReceiver.java
@@ -1,14 +1,14 @@
 package org.example.jmscalculator;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.jmscalculator.request.CalculatorRequest;
-import org.example.jmscalculator.response.CalculatorResponse;
 import org.example.jmscalculator.response.ErrorResponse;
 import org.example.jmscalculator.service.CalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.JmsHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -24,15 +24,18 @@ public class JmsReceiver {
     private JmsTemplate jmsTemplate;
 
     @JmsListener(destination = "org.example.jms-calculator.request")
-    public void receiveMessage(String message) {
+    public void receiveMessage(String body, @Header(JmsHeaders.CORRELATION_ID) String jmsCorrelationId) {
         Object response;
         try {
-            CalculatorRequest calculatorRequest = objectMapper.readValue(message, CalculatorRequest.class);
+            CalculatorRequest calculatorRequest = objectMapper.readValue(body, CalculatorRequest.class);
             response = calculatorService.calculate(calculatorRequest);
         } catch (Exception e) {
             System.out.println("Exception occurred: " + e.getMessage());
             response = new ErrorResponse(e.getMessage());
         }
-        jmsTemplate.convertAndSend("org.example.jms-calculator.response", response);
+        jmsTemplate.convertAndSend("org.example.jms-calculator.response", response, message -> {
+            message.setJMSCorrelationID(jmsCorrelationId);
+            return message;
+        });
     }
 }
